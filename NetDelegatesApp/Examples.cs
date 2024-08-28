435366fb11ead2bb8db262eb11bcfbd12c5ebd27
Максим Ефимов <efimov_m@t.top-academy.ru>
2024-08-28 21:13:50 +0300

    Добавьте файлы проекта.

---
+++ NetDelegatesApp/Examples.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetDelegatesApp
+{
+    delegate int Oper(int x, int y);
+    delegate void MessageOut();
+    delegate int AccOperation(ref int begin, int x, int y);
+    delegate void PrintMessage(string message);
+    delegate int IntOpers(int a, int b);
+    delegate double DoubleOpers(double a, double b);
+    delegate T Opers<T>(T a, T b);
+
+
+    class MathOpers
+    {
+        public static int Sum(int a, int b) { return a + b; }
+        public static double Sum(double a, double b) { return a + b; }
+        public static int Mult(int a, int b) { return a * b; }
+        public static double Mult(double a, double b) { return a * b; }
+    }
+
+    static class Examples
+    {
+        public static void DelegateWelcome()
+        {
+            Oper operation = Add;
+            operation = new Oper(Mult);
+
+            Console.WriteLine(operation(10, 20)); // Mult(10, 20)
+
+            MessageOut mout = PrintHello;
+            mout(); // PrintHello();
+
+            Calc(Mult, PrintHello, 20, 30);
+
+            int Calc(Oper operation, MessageOut printer, int a, int b)
+            {
+                printer();
+                return operation(a, b);
+            }
+
+            int Add(int a, int b)
+            {
+                return a + b;
+            }
+
+            int Mult(int a, int b)
+            {
+                return a * b;
+            }
+
+            void PrintHello()
+            {
+                Console.WriteLine("Hello world");
+            }
+
+            
+        }
+
+        public static void DelegateInvoke()
+        {
+            string message = "Hello world";
+
+            PrintMessage? printer;
+            printer = PrintGreen;
+            //printer += PrintRed;
+            printer(message);
+            Console.WriteLine();
+
+            printer -= PrintGreen;
+            //if(printer is not null)
+            //    printer(message);
+            printer?.Invoke(message);
+
+
+            //PrintMessage printGreen = PrintGreen;
+            //PrintMessage printRed = PrintRed;
+            //PrintMessage printRedGreen = printGreen + printRed;
+
+
+            AccOperation operation = Sum;
+            operation += Mult;
+            int begin = 0;
+            Console.WriteLine(operation.Invoke(ref begin, 20, 30));
+            Console.WriteLine(begin);
+
+
+            void PrintGreen(string message)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(message);
+            }
+
+            void PrintRed(string message)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(message);
+            }
+
+            int Sum(ref int begin, int a, int b)
+            {
+                begin = begin + a + b;
+                return a + b;
+            }
+            int Mult(ref int begin, int a, int b)
+            {
+                begin = begin * a * b;
+                return a * b;
+            }
+        }
+
+        public static void DelegateGeneric()
+        {
+            IntOpers intOpers = MathOpers.Sum;
+            DoubleOpers doubleOpers = MathOpers.Sum;
+
+            Opers<int> intOpersT = MathOpers.Sum;
+            Opers<double> doubleOpersT = MathOpers.Sum;
+
+
+
+            Calc(MathOpers.Sum, 10.0, 20);
+
+
+            int IntCalc(IntOpers oper, int a, int b)
+            {
+                return oper(a, b);
+            }
+
+            int CalcT(Opers<int> oper, int a, int b)
+            {
+                return oper(a, b);
+            }
+
+            T Calc<T>(Opers<T> oper, T a, T b)
+            {
+                return oper(a, b);
+            }
+
+            IntOpers? GetOper(char sign)
+            {
+                if (sign == '+') return MathOpers.Sum;
+                else if (sign == '*') return MathOpers.Mult;
+                else return null;
+            }
+        }
+    }
+}
---
+++ NetDelegatesApp/Program.cs
@@ -0,0 +1,62 @@
+﻿BankAccount accaunt = new(1000);
+accaunt.RegisterHandler(PrintGreen);
+
+accaunt.Take(500);
+accaunt.Take(400);
+
+accaunt.RegisterHandler(PrintRed);
+
+accaunt.Take(200);
+
+
+void PrintRed(string message)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(message);
+}
+
+void PrintGreen(string message)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine(message);
+}
+
+
+public delegate void AccountHandler(string message);
+
+public class BankAccount
+{
+    int amount;
+    AccountHandler? accountHandler;
+
+    public void RegisterHandler(AccountHandler handler)
+    {
+        accountHandler += handler;
+    }
+
+    public BankAccount(int amount)
+    {
+        this.amount = amount;
+        this.accountHandler = null;
+    }
+
+    public void Add(int amount)
+    {
+        this.amount += amount;
+        accountHandler?.Invoke($"На счет доблено {amount} руб. Общая сумма {this.amount}");
+    }
+
+    public void Take(int amount)
+    {
+        if(amount < this.amount)
+        {
+            this.amount -= amount;
+            accountHandler?.Invoke($"Со счета снято {amount} руб. Общая сумма {this.amount}");
+        }
+        else
+            accountHandler?.Invoke("На счете не зватает средств");
+
+    }
+}
+
+
